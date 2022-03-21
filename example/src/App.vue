ace630672f1b70a33a6e9cc600c6af7d4826392a
kory <kory.lee@qq.com>
2022-03-22 00:11:20 +0800

    docs: example

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
+import Variation from "./components/Variation.vue";
+const variations = [{ name: "颜色", options: ["red", "green", "blue"] }];
 </script>
 
 <template>
-<div>test</div>
+  <div>
+    <Variation v-for="(variation, index) of variations" :key="index" :value="variation" />
+  </div>
 </template>
---
+++ example/src/components/Variation.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps({
+  value: {
+    type: Object,
+    default: () => ({
+      name: "",
+      options: [],
+    }),
+  },
+});
+</script>
+<template>
+  <div style="border: 1px solid black">
+    <div>name: {{ value.name }}</div>
+    <div>
+      options:
+      <button v-for="(option, index) of value.options" :key="index" class="ml-2">{{ option }}</button>
+      <button class="ml-2">+</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.ml-2{
+  margin-left: 2px;
+}
+</style>
