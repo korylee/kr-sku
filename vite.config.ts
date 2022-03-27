8a7543275da897d81f624a64b17e022d1837e028
kory <kory.lee@qq.com>
2022-03-27 16:51:01 +0800

    chore: add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,7 @@ export default defineConfig(({ mode }) => {
     };
   }
   return {
+    base: mode==='gh-pages'?'sku':'/',
     resolve: {
       alias: {
         "@korylee/sku": resolve(__dirname, "src/index.ts"),
